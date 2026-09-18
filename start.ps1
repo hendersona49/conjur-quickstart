9e6c0da5b1764124702e0881e2e93c22a3959957
Adrian Henderson <hendersona49@hotmail.com>
2026-09-18 10:10:50 -0400

    Enhance Conjur lab with identity management and dynamic host provisioning

--- start.ps1
+++ start.ps1
@@ -1,3 +1,9 @@
-$env:CONJUR_DATA_KEY = Get-Content -Raw data_key
+$env:CONJUR_DATA_KEY = (Get-Content -Raw .\data_key).Trim()
+
 docker compose up -d
+
+Start-Sleep -Seconds 10
+
+docker compose exec client conjur init oss -u https://proxy -a myConjurAccount --self-signed --force
+
 docker ps
